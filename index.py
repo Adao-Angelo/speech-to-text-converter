9ada911d2943846624244677c46bad8108fa04d6
Adao-Angelo <adaobegginer@gmail.com>
2024-04-25 00:00:26 +0100

    config script for converte sheeth in text

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 import speech_recognition as sr
+import pyttsx3
 
 
 def recognize_speech():
+  
     microphone = sr.Recognizer()
     with sr.Microphone() as source:
         microphone.adjust_for_ambient_noise(source)
-        print("Hello!")
+        print("listening ...")
         audio = microphone.listen(source)
         try:
             phrase = microphone.recognize_google(audio,language="pt-BR")
